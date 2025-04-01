b33e094c6238ac1d41837b0ec672468c01597cd7
Pawan Tiwari <ramtiwari7081@gmail.com>
2025-04-02 02:38:14 +0530

    add portfolio link

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -22,44 +22,16 @@ const Projects = () => {
             title="Codenest Platform"
             category="MERN Website"
             image={workImgThree} // Replace with actual image later
+            link="https://codenest-arww.onrender.com/"
           />
           <ProjectsCard
-            title="Portfolio Website"
-            category="Personal Branding"
+            title="BookNest"
+            category="MERN Website"
             image={workImgOne}
+             link="https://codenest-arww.onrender.com/"
           />
-          <ProjectsCard
-            title="Automated Attendance System"
-            category="Machine Learning"
-            image={workImgTwo}
-          />
-          <ProjectsCard
-            title="E-Learning App"
-            category="Mobile Application"
-            image={workImgFive}
-          />
-        </div>
-        <div className="px-6">
-          <ProjectsCard
-            title="E-commerce Platform"
-            category="Web Development"
-            image={workImgFour}
-          />
-          <ProjectsCard
-            title="Custom Admin Dashboard"
-            category="Full Stack"
-            image={workImgSix}
-          />
-          <ProjectsCard
-            title="AI Chatbot"
-            category="Artificial Intelligence"
-            image={workImgEight}
-          />
-          <ProjectsCard
-            title="Brand Logo Design"
-            category="Graphic Design"
-            image={workImgSeven}
-          />
+        
+         
         </div>
       </div>
     </div>
--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-const ProjectsCard = ({ image, title, category }) => {
+const ProjectsCard = ({ image, title, category, link }) => {
   return (
-    <div className="w-full py-8 flex flex-col justify-center items-center border-b-[1px] border-b-zinc-800">
-      <div className="w-full h-full mb-3 overflow-hidden relative cursor-pointer group">
+    <div
+      className="w-full py-8 flex flex-col justify-center items-center border-b-[1px] border-b-zinc-800 cursor-pointer"
+      onClick={() => window.open(link, "_blank")}
+    >
+      <div className="w-full h-full mb-3 overflow-hidden relative group">
         <img
-          className="w-full h-full object-cover scale-100 group-hover:scale-110 duration-300 cursor-pointer"
+          className="w-full h-full object-cover scale-100 group-hover:scale-110 duration-300"
           src={image}
-          alt="ImageOne"
+          alt={title}
         />
         <div className="w-full h-full absolute top-0 left-0 hover:bg-gradient-to-r from-green-600 via-green-600 to-green-200 opacity-20"></div>
       </div>
